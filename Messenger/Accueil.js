1954f40b3c80f62004b7eca46a5cb74d41743802
bleaz01 <ridjea@gmail.com>
2019-11-30 01:06:56 +0100

    setup project, page acceuil

---
+++ Messenger/Accueil.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native';
+
+export default Accueil = () =>{
+
+    return(
+        <View>
+            <Text>
+                Accueil Logo Image
+            </Text>
+        </View>
+    )
+}
